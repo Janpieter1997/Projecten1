9b6c0aeccebb0c40563b7535ead6d0ab4eec4c38
Janpieter Thomas <janpieter.thomas@student.odisee.be>
2017-04-03 20:10:00 +0200

    New repo

---
+++ Code/Twitter/flask-oauth-example-master/app.py
@@ -0,0 +1,87 @@
+from flask import Flask, redirect, url_for, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user,\
+    current_user
+from oauth import OAuthSignIn
+##https://blog.miguelgrinberg.com/post/oauth-authentication-with-flask
+refreshed = False;
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['OAUTH_CREDENTIALS'] = {
+    'facebook': {
+        'id': '1639495716359523',
+        'secret': '56b71cf5fda412349ee812188573180d'
+    },
+    'twitter': {
+        'id': 'n4FPdDAtVvltcdDDQI9ckVf0z',
+        'secret': 'a8KlTq42QmGEWz8tlpyIllJPpywxHnCHUZGpjbpFsqKt0vTN5A'
+    }
+}
+
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.login_view = 'index'
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+@app.route('/refresh')
+def refresh():
+    oauth = OAuthSignIn.get_provider('twitter')
+    global refreshed
+    refreshed = True;
+    return oauth.authorize()
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+    if not current_user.is_anonymous:
+        return redirect(url_for('index'))
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    global refreshed
+    if not refreshed:
+        if not current_user.is_anonymous:
+            return redirect(url_for('index'))
+    refreshed = False;
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+    if social_id is None:
+        flash('Authentication failed.')
+        return redirect(url_for('index'))
+    user = User.query.filter_by(social_id=social_id).first()
+    if not user:
+        user = User(social_id=social_id, nickname=username, email=email)
+        db.session.add(user)
+        db.session.commit()
+    login_user(user, True)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
